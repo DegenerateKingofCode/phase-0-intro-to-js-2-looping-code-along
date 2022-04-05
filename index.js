2d82f6f1d8acd5fc62d95b21b19bd93ec53f2e6c
Tanner <Tanner@tleroy.com>
2022-04-05 19:17:37 -0400

    commit tree 389e500337af7634184b184d2fa7959a1481e363

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+
+    let writeCardsDone = []
+    for (let i = 0; i < names.length; i++){
+        writeCardsDone.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+        console.log(writeCardsDone)
+    }
+    return writeCardsDone
+}
+    
+
+function countDown(){
+    let countDown = 10
+    while (countDown > -1) {
+        console.log(countDown)
+        countDown--
+      
+    }
+}
